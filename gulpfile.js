ad5570bba4c5802d5d238fa58b7a9b84a0133395
Jan T. Sott <jan@idleberg.com>
2016-07-22 20:01:25 +0200

    update file

--- gulpfile.js
+++ gulpfile.js
@@ -2,63 +2,82 @@
  * sublimetext-gulpfile.js
  * https://github.com/idleberg/sublimetext-gulpfile.js
  *
- * Copyright (c) 2014 Jan T. Sott
+ * Copyright (c) 2014-2016 Jan T. Sott
  * Licensed under the MIT license.
  */
 
+// Dependencies
 var gulp = require('gulp'),
     debug = require('gulp-debug'),
     jsonlint = require('gulp-json-lint');
+    ymlVal = require('gulp-yaml-validate');
+    xmlVal = require('gulp-xml-validator');
 
+// Supported files
+var jsonFiles = [
+    '**/*.JSON-sublime-syntax',
+    '**/*.JSON-tmLanguage',
+    '**/*.JSON-tmTheme',
+    '**/*.sublime-build',
+    '**/*.sublime-commands',
+    '**/*.sublime-completions',
+    '**/*.sublime-keymap',
+    '**/*.sublime-macro',
+    '**/*.sublime-menu',
+    '**/*.sublime-settings',
+    '**/*.sublime-theme',
+    'messages.json'
+];
 
-gulp.task('default', ['json', 'xml']);
+var xmlFiles = [
+    '**/*.plist',
+    '**/*.PLIST-sublime-syntax',
+    '**/*.PLIST-tmLanguage',
+    '**/*.PLIST-tmTheme',
+    '**/*.sublime-snippet',
+    '**/*.tmCommand',
+    '**/*.tmLanguage',
+    '**/*.tmPreferences',
+    '**/*.tmSnippet',
+    '**/*.tmTheme',
+    '**/*.xml',
+    '*.bbcolors',
+    '*.dvtcolortheme',
+    '*.icls',
+    '*.itermcolors',
+    '*.terminal'
+];
 
-gulp.task('json', function(){
-      gulp.src([
-            '**/*.JSON-tmLanguage',
-            '**/*.sublime-build',
-            '**/*.sublime-commands',
-            '**/*.sublime-completions',
-            '**/*.sublime-keymap',
-            '**/*.sublime-macro',
-            '**/*.sublime-menu',
-            '**/*.sublime-settings',
-            '**/*.sublime-theme',
-            'messages.json'
-        ])
-        .pipe(debug({verbose: true}))
-        .pipe(jsonlint())
-        .pipe(jsonlint.report('verbose'));
+var ymlFiles = [
+    '**/*.sublime-syntax',
+    '**/*.YAML-tmLanguage',
+    '**/*.YAML-tmTheme'
+];
+
+// Exclude node_modules
+var self = '!node_modules/**/*';
+
+// Available tasks
+gulp.task('default', ['lint']);
+gulp.task('lint', ['lint:json', 'lint:xml', 'lint:yml']);
+
+// Lint JSON
+gulp.task('lint:json', function(){
+  return gulp.src(jsonFiles, self)
+    .pipe(debug({title: 'lint:json'}))
+    .pipe(jsonlint())
 });
 
-gulp.task('xml', function() {
-    gulp.src([
-            '**/*.plist',
-            '**/*.sublime-snippet',
-            '**/*.tmCommand',
-            '**/*.tmLanguage',
-            '**/*.tmPreferences',
-            '**/*.tmSnippet',
-            '**/*.tmTheme',
-            '**/*.xml'
-        ])
-    console.log('XML linting not yet supported');
+// Validate XML
+gulp.task('lint:xml', function() {
+  return gulp.src(xmlFiles, self)
+    .pipe(debug({title: 'lint:xml'}))
+    .pipe(xmlVal());
 });
 
-gulp.task('watch', function () {
-   gulp.watch([
-            '**/*.JSON-tmLanguage',
-            '**/*.sublime-build',
-            '**/*.sublime-commands',
-            '**/*.sublime-completions',
-            '**/*.sublime-keymap',
-            '**/*.sublime-macro',
-            '**/*.sublime-menu',
-            '**/*.sublime-settings',
-            '**/*.sublime-theme',
-            'messages.json',
-            '!node_modules/**/*.*'
-         ],
-         ['default'])
+// Validate YAML
+gulp.task('lint:yml', function() {
+  return gulp.src(ymlFiles, self)
+    .pipe(debug({title: 'lint:yml'}))
+    .pipe(ymlVal({ safe: true }));
 });
-
