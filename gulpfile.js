292be4c226deec1a339a4eba62f6456d88a10584
idleberg <jan@fragnebenan.com>
2018-01-05 12:11:39 +0100

    version bump

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * sublimetext-gulpfile.js
  * https://github.com/idleberg/sublimetext-gulpfile.js
  *
- * Copyright (c) 2014-2016 Jan T. Sott
+ * Copyright (c) 2014-2018 Jan T. Sott
  * Licensed under the MIT license.
  */
 
@@ -15,70 +15,73 @@ var gulp = require('gulp'),
 
 // Supported files
 var jsonFiles = [
-    './**/*.JSON-sublime-syntax',
-    './**/*.JSON-tmLanguage',
-    './**/*.JSON-tmTheme',
-    './**/*.sublime-build',
-    './**/*.sublime-commands',
-    './**/*.sublime-completions',
-    './**/*.sublime-keymap',
-    './**/*.sublime-macro',
-    './**/*.sublime-menu',
-    './**/*.sublime-settings',
-    './**/*.sublime-theme',
-    './messages.json',
-    '!node_modules/**/*.json'
+    '**/*.JSON-sublime-syntax',
+    '**/*.JSON-tmLanguage',
+    '**/*.JSON-tmTheme',
+    '**/*.sublime-build',
+    '**/*.sublime-commands',
+    '**/*.sublime-completions',
+    '**/*.sublime-keymap',
+    '**/*.sublime-macro',
+    '**/*.sublime-menu',
+    '**/*.sublime-settings',
+    '**/*.sublime-theme',
+    'messages.json',
+    '!node_modules/**/*'
 ];
 
 var xmlFiles = [
-    './**/*.plist',
-    './**/*.PLIST-sublime-syntax',
-    './**/*.PLIST-tmLanguage',
-    './**/*.PLIST-tmTheme',
-    './**/*.sublime-snippet',
-    './**/*.tmCommand',
-    './**/*.tmLanguage',
-    './**/*.tmPreferences',
-    './**/*.tmSnippet',
-    './**/*.tmTheme',
-    './**/*.xml',
-    '!node_modules/**/*.xml'
+    '**/*.plist',
+    '**/*.PLIST-sublime-syntax',
+    '**/*.PLIST-tmLanguage',
+    '**/*.PLIST-tmTheme',
+    '**/*.sublime-snippet',
+    '**/*.tmCommand',
+    '**/*.tmLanguage',
+    '**/*.tmPreferences',
+    '**/*.tmSnippet',
+    '**/*.tmTheme',
+    '**/*.xml',
+    '!node_modules/**/*'
 ];
 
 var ymlFiles = [
-    './**/*.sublime-syntax',
-    './**/*.YAML-tmLanguage',
-    './**/*.YAML-tmTheme',
-    '!node_modules/**/*.yml'
+    '**/*.sublime-syntax',
+    '**/*.YAML-tmLanguage',
+    '**/*.YAML-tmTheme',
+    '!node_modules/**/*'
 ];
 
 // Lint JSON
-gulp.task('lint:json', gulp.series(function(done) {
+gulp.task('lint:json', (done) =>{
   gulp.src(jsonFiles)
     .pipe(debug({title: 'lint:json'}))
     .pipe(jsonLint())
     .pipe(jsonLint.failAfterError())
     .pipe(jsonLint.reporter());
+
   done();
-}));
+});
 
 // Validate XML
-gulp.task('lint:xml', gulp.series(function(done) {
+gulp.task('lint:xml', (done) => {
   gulp.src(xmlFiles)
     .pipe(debug({title: 'lint:xml'}))
     .pipe(xmlVal());
+
   done();
-}));
+});
 
 // Validate YAML
-gulp.task('lint:yml', gulp.series(function(done) {
+gulp.task('lint:yml', (done) => {
   gulp.src(ymlFiles)
     .pipe(debug({title: 'lint:yml'}))
     .pipe(ymlVal({ safe: true }));
+
   done();
-}));
+});
 
 // Available tasks
-gulp.task('lint', gulp.parallel('lint:json', 'lint:xml', 'lint:yml', function(done) {
+gulp.task('default', gulp.parallel('lint:json', 'lint:xml', 'lint:yml', (done) => {
   done();
 }));
